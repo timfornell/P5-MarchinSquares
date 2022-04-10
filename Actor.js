cc71d5778b220161e82bc1f9e4c57b9df7111464
Vincent <tim.fornell@hotmail.com>
2022-04-10 17:37:23 +0200

    Simple collision handling implemented

--- Actor.js
+++ Actor.js
@@ -31,55 +31,59 @@ class Actor {
          yDirection = 1;
       }
 
-      var cells = this.getVectorToClosestLine(surroundingCells);
-
-      var canMove = true;
-      for (let c of cells) {
-         stroke(0);
-         line(this.position.x, this.position.y, c.vectorToClosestPoint.x, c.vectorToClosestPoint.y);
-      }
-
-      if (canMove) {
-         this.position.add(xDirection * this.speed, yDirection * this.speed);
-      }
+      this.tryToMove(surroundingCells, xDirection, yDirection);
    }
 
-   getVectorToClosestLine(surroundingCells) {
-      var cells = [];
+   tryToMove(surroundingCells, xDirection, yDirection) {
+      var desiredPosition = this.position.copy();
+      desiredPosition.add(xDirection * this.speed, yDirection * this.speed);
 
       for (let cell of surroundingCells) {
          for (let gridLine of cell.lines) {
-            var lineNormalDotproduct = this.getDistanceToClosestPointOnLine(gridLine, this.position);
-            var distance = abs(lineNormalDotproduct);
+            var closestPointOnLine = this.getVectorToClosestPointOnLine(gridLine, this.position);
+            line(this.position.x, this.position.y, closestPointOnLine.x, closestPointOnLine.y);
 
-            var lineVector = createVector(gridLine.end.x - gridLine.start.x, gridLine.end.y - gridLine.start.y);
-            var angle = PI / 2 + this.position.angleBetween(lineVector);
-            var v1 = this.position.copy();
-            v1.setMag(distance);
+            if ((desiredPosition.x - closestPointOnLine.x) ** 2 + (desiredPosition.y - closestPointOnLine.y) ** 2 <= (this.size / 2) ** 2)
+            {
+               // Can't move to the exact position, try to find another that moves in the wanted direction
+               return;
+            } else {
 
-            if (lineNormalDotproduct < 0) {
-               v1.rotate(angle);
-            } else if (lineNormalDotproduct > 0) {
-               v1.rotate(PI + angle);
             }
-
-            v1.add(this.position);
-            var lineSize = gridLine.start.dist(gridLine.end);
-            var d1 = v1.dist(gridLine.start);
-            var d2 = v1.dist(gridLine.end);
-            if (d1 >= lineSize || d2 >= lineSize) {
-               if (d1 > d2) {
-                  v1 = createVector(gridLine.end.x, gridLine.end.y);
-               } else {
-                  v1 = createVector(gridLine.start.x, gridLine.start.y);
-               }
-            }
-
-            cells.push({cell: cell, vectorToClosestPoint: v1});
          }
       }
 
-      return cells;
+      this.position = desiredPosition.copy();
+   }
+
+   getVectorToClosestPointOnLine(gridLine, position) {
+      var lineVector = createVector(gridLine.end.x - gridLine.start.x, gridLine.end.y - gridLine.start.y);
+
+      var lineNormalDotproduct = this.getDistanceToClosestPointOnLine(gridLine, position);
+      var distance = abs(lineNormalDotproduct);
+      var angle = PI / 2 + this.position.angleBetween(lineVector);
+      var v1 = position.copy();
+      v1.setMag(distance);
+
+      if (lineNormalDotproduct < 0) {
+         v1.rotate(angle);
+      } else if (lineNormalDotproduct > 0) {
+         v1.rotate(PI + angle);
+      }
+
+      v1.add(this.position);
+      var lineSize = gridLine.start.dist(gridLine.end);
+      var d1 = v1.dist(gridLine.start);
+      var d2 = v1.dist(gridLine.end);
+      if (d1 >= lineSize || d2 >= lineSize) {
+         if (d1 > d2) {
+            v1 = createVector(gridLine.end.x, gridLine.end.y);
+         } else {
+            v1 = createVector(gridLine.start.x, gridLine.start.y);
+         }
+      }
+
+      return v1;
    }
 
    getDistanceToClosestPointOnLine(gridLine, point) {
