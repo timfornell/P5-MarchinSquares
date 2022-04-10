175d4481bdd843da3d3f6f3b089095601059a116
Vincent <tim.fornell@hotmail.com>
2022-04-10 16:26:09 +0200

    Tidy up some code

--- Actor.js
+++ Actor.js
@@ -31,12 +31,12 @@ class Actor {
          yDirection = 1;
       }
 
-      var vectors = this.getVectorToClosestLine(surroundingCells);
+      var cells = this.getVectorToClosestLine(surroundingCells);
 
       var canMove = true;
-      for (let v of vectors) {
+      for (let c of cells) {
          stroke(0);
-         line(this.position.x, this.position.y, v.x, v.y);
+         line(this.position.x, this.position.y, c.vectorToClosestPoint.x, c.vectorToClosestPoint.y);
       }
 
       if (canMove) {
@@ -45,20 +45,12 @@ class Actor {
    }
 
    getVectorToClosestLine(surroundingCells) {
-      var closestVectors = [];
+      var cells = [];
 
       for (let cell of surroundingCells) {
          for (let gridLine of cell.lines) {
-            var x0 = this.position.x;
-            var y0 = this.position.y;
-            var x1 = gridLine.end.x;
-            var y1 = gridLine.end.y;
-            var x2 = gridLine.start.x;
-            var y2 = gridLine.start.y;
-
-            // https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located
-            var lineNormalDotproduct = (x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1);
-            var distance = abs(lineNormalDotproduct) / sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+            var lineNormalDotproduct = this.getDistanceToClosestPointOnLine(gridLine, this.position);
+            var distance = abs(lineNormalDotproduct);
 
             var lineVector = createVector(gridLine.end.x - gridLine.start.x, gridLine.end.y - gridLine.start.y);
             var angle = PI / 2 + this.position.angleBetween(lineVector);
@@ -83,10 +75,22 @@ class Actor {
                }
             }
 
-            closestVectors.push(v1);
+            cells.push({cell: cell, vectorToClosestPoint: v1});
          }
       }
 
-      return closestVectors;
+      return cells;
+   }
+
+   getDistanceToClosestPointOnLine(gridLine, point) {
+      var x0 = point.x;
+      var y0 = point.y;
+      var x1 = gridLine.end.x;
+      var y1 = gridLine.end.y;
+      var x2 = gridLine.start.x;
+      var y2 = gridLine.start.y;
+
+      // https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located
+      return ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1)) / sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
    }
 }
